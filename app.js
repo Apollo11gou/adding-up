1f2941961be26702e2b44d7c8f131121927a8d5f
Apollo11gou <next.soharuna@gmail.com>
2021-03-20 16:07:53 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const readline = require("readline");
 const rs = fs.createReadStream("./popu-pref.csv");
 const rl = readline.createInterface({ input: rs, output: {} });
+const prefectureDataMap = new Map(); //key:都道府県 value:集計データのオブジェクト
 // lineString には、読み込んだ 1 行の文字列が入っている
 rl.on("line", (lineString) => {
   // lineString で与えられた文字列をカンマ , で分割
@@ -14,8 +15,46 @@ rl.on("line", (lineString) => {
   const prefecture = columns[1];
   const popu = parseInt(columns[3]);
   if (year === 2010 || year === 2015) {
-    console.log(year);
-    console.log(prefecture);
-    console.log(popu);
+    let value = prefectureDataMap.get(prefecture);
+    //popu10 が 2010 年の人口、 popu15 が 2015 年の人口、 change が人口の変化率を表すプロパティ
+    //value の値が Falsy の場合に、value に初期値となるオブジェクトを代入
+    if (!value) {
+      value = {
+        popu10: 0,
+        popu15: 0,
+        change: null,
+      };
+    }
+    if (year === 2010) {
+      value.popu10 = popu;
+    }
+    if (year === 2015) {
+      value.popu15 = popu;
+    }
+    prefectureDataMap.set(prefecture, value);
   }
+});
+// 'close' イベントは、全ての行を読み込み終わった際に呼び出される。
+// その際の処理として、各県各年男女のデータが集計された Map のオブジェクトを出力
+rl.on("close", () => {
+  for (let [key, value] of prefectureDataMap) {
+    // 集計データのオブジェクト value の change プロパティに、変化率を代入するコード
+    value.change = value.popu15 / value.popu10;
+  }
+  // 連想配列を普通の配列に変換する処理
+  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+    return pair2[1].change - pair1[1].change;
+  });
+  const rankingStrings = rankingArray.map(([key, value]) =>{
+    return(
+    key +
+    ':' +
+    value.popu10 +
+    '=>' +
+    value.popu15 +
+    '変化率:' +
+    value.change
+  );
+  });
+  console.log(rankingStrings);
 });
