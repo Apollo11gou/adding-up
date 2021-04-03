7adacc74731cf66817212345b240a4f1eca3cee7
Apollo11gou <next.soharuna@gmail.com>
2021-04-03 16:18:31 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const readline = require("readline");
 const rs = fs.createReadStream("./popu-pref.csv");
 const rl = readline.createInterface({ input: rs, output: {} });
-const prefectureDataMap = new Map();// key: 都道府県 value: 集計データのオブジェクト
+const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on("line", (lineString) => {
   //引数lineStringを分割する
   const columns = lineString.split(",");
@@ -13,27 +13,42 @@ rl.on("line", (lineString) => {
   const popu = parseInt(columns[3]);
   if (year === 2010 || year === 2015) {
     let value = prefectureDataMap.get(prefecture);
-    if (!value){
-      value ={
+    if (!value) {
+      value = {
         popu10: 0,
         popu15: 0,
-        change: null
+        change: null,
       };
     }
-    if (year ===2010){
+    if (year === 2010) {
       value.popu10 = popu;
     }
-    if (year === 2015){
+    if (year === 2015) {
       value.popu15 = popu;
     }
     prefectureDataMap.set(prefecture, value);
   }
 });
-rl.on('close', () =>{
+rl.on("close", () => {
   //'close' イベントは全ての行を読み込み終わった際に呼び出される。
   //for-of構文を使用し
-  for (let [key, value] of prefectureDataMap){
+  for (let [key, value] of prefectureDataMap) {
     value.change = value.popu15 / value.popu10;
   }
-  console.log(prefectureDataMap)
+  //連想配列を普通の配列に変換する処理
+  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+
+    return pair2[1].change - pair1[1].change;
+  });
+  const rankingStrings =rankingArray.map(([key,value]) =>{
+    return(
+      key + ':' +
+      value.popu10 +
+      '=>' +
+      value.popu15 +
+      '変化率:' +
+      value.change
+    );
+  });
+  console.log(rankingStrings);
 });
